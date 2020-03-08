826af7838138ff8f2b50d595a8f555f6bd05a860
Kevin Sartori <kevin.bouty@hotmail.fr>
2020-03-08 19:21:02 +0100

    added command line parser

--- Checkpoint2/Program.cs
+++ Checkpoint2/Program.cs
@@ -2,21 +2,49 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Globalization;
+using CommandLine;
+
 
 namespace WCS
 {
     class Program
     {
-        public static void Main()
+        [Verb("event", HelpText = "Get all event from a person id into a time interval")]
+        class EventFromPersonOptions
         {
-            Event newEvent = new Event("Important meeting", DateTime.Now, DateTime.Now);
+            [Option('i', "personId", Required = true, HelpText = "Person id (1 to 225)")]
+            public int PersonId { get; set; }
+
+            [Option('s', "startDate", Required = true, HelpText = "Event startDate format yyyyMMdd (20200529)")]
+            public string StartDate { get; set; }
+
+            [Option('e', "endDate", Required = true, HelpText = "Event end Date format yyyyMMdd (20200530)")]
+            public string EndDate { get; set; }
+
+        }
+
+        [Verb("quests", HelpText = "Get all quests from a Cursus")]
+        class QuestFromCursus
+        {
+            [Option('n', "cursusName", Required = true, HelpText = "Name of the cursus (Cursus 1)")]
+            public string cursusName { get; set; }
+        }
+
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<EventFromPersonOptions, QuestFromCursus>(args)
+                            .WithParsed<EventFromPersonOptions>(RunEventFromPersonOption)
+                            .WithParsed<QuestFromCursus>(RunQuestFromCursus);
+
+
+            /*Event newEvent = new Event("Important meeting", DateTime.Now, DateTime.Now);
             newEvent.StartTime = DateTime.Now;
             newEvent.EndTime = DateTime.Now + TimeSpan.FromHours(1);
             newEvent.Postpone(TimeSpan.FromHours(1));
             Console.WriteLine("Another meeting is postponed");
 
 
-
+            
             // Affiche tous les événements d'une personne
             string date = "20200529";
             string date2 = "20200530";
@@ -27,8 +55,8 @@ namespace WCS
             int personId = 3;
 
             Database.GetInstance().GetEventFromPerson(personId, startDate, endDate);
-
-
+            
+            
             // Affiche toutes les quêtes d'un cursus
             string cursusName = "Cursus 1";
             List<Quest> quests = Database.GetInstance().GetQuestFromCursus(cursusName);
@@ -39,7 +67,7 @@ namespace WCS
                 Console.WriteLine();
                 Console.Write(quest.Title + "\t");
                 Console.Write(quest.Text);
-            }
+            }*/
 
 
             /*
@@ -47,7 +75,27 @@ namespace WCS
             Console.WriteLine("Get all students from a cursus");
             string cursusName = "Cursus 1";
             Database.GetInstance().GetStudentsFromCursus(cursusName);*/
-            
+
+        }
+
+        static void RunEventFromPersonOption(EventFromPersonOptions options)
+        {
+            DateTime startDate = DateTime.ParseExact(options.StartDate, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None);
+            DateTime endDate = DateTime.ParseExact(options.EndDate, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None);
+            Database.GetInstance().GetEventFromPerson(options.PersonId, startDate, endDate);
+        }
+
+        static void RunQuestFromCursus(QuestFromCursus options)
+        {
+            List<Quest> quests = Database.GetInstance().GetQuestFromCursus(options.cursusName);
+
+            Console.Write("Quest title \t Quest text");
+            foreach (Quest quest in quests)
+            {
+                Console.WriteLine();
+                Console.Write(quest.Title + "\t");
+                Console.Write(quest.Text);
+            }
         }
     }
 }
